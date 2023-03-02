18f6a9f69b64b72e588ebde17e88eac89b2404b8
MuhAsmann <muhasmangit@gmail.com>
2023-03-02 21:05:26 +0700

    Menerapkan webpack

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// import main from "./script/view/main.js";
+// import './script/component/app-bar.js';
+
+
+// document.addEventListener('DOMContentLoaded', main);    
+
+import './styles/style.css';
+import './script/component/app-bar.js';
+import main from './script/view/main.js';
+ 
+document.addEventListener('DOMContentLoaded', main);
